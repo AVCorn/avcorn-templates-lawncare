55fefad9248d0d4fe8560a6c810cc9f4f626b638
Benjamin J. Young <ben@blaher.me>
2023-12-24 11:05:36 -0500

    config file update

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Lawncare Template Configuration
+ *
+ * @phpversion >= 8.1
+ * @package     Application
+ * @subpackage  Configuration
+ */
+
+$config['info']['company'] = "Nutty's Lawnscape";
+$config['info']['title'] = "Nutty's Lawnscape";
+$config['info']['motto'] = 'Mowing down the competition!';
+$config['info']['slogan'] = 'Mowing down the competition!';

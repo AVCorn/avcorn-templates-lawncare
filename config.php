6d99e5bd25217ff4dd3f22dae434325a709505b2
Benjamin J. Young <ben@blaher.me>
2024-01-08 08:36:57 -0500

    Formatting config

--- config.php
+++ config.php
@@ -20,6 +20,6 @@ declare(strict_types=1);
  * @var array $config Configuration
  */
 $config['info']['company'] = "Nutty's Lawnscape";
-$config['info']['title'] = "Nutty's Lawnscape";
-$config['info']['motto'] = 'Mowing down the competition!';
-$config['info']['slogan'] = 'Mowing down the competition!';
+$config['info']['title']   = "Nutty's Lawnscape";
+$config['info']['motto']   = 'Mowing down the competition!';
+$config['info']['slogan']  = 'Mowing down the competition!';
